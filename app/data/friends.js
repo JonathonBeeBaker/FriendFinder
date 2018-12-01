479719396fb3e980aa67b06348ee6d8d102a0140
JonathonBee <jonathonbeebaker@gmail.com>
2018-12-01 11:46:03 -0700

    new

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+
+//Friend Data Below
+
+var friends = [
+    {name: "Bob Ross",
+    photo:"https://images05.military.com/sites/default/files/styles/full/public/media/people/2013/05/bobrosspainting.jpg?itok=A3G_e8MM",
+    scores :["5","2","3","4","2","4","1","3","5","4"]},
+    {name: "Deadpool",
+    photo: "https://pmcvariety.files.wordpress.com/2017/11/deadpool-2.jpg",
+    scores :["5","5","4","2","3","1","4","5","2","5"]},
+    {name: "Jack Black",
+    photo: "https://rtvc-assets-radionica3.s3.amazonaws.com/s3fs-public/styles/image_750x422/public/field/image/article/jack_black_tenacious_d.jpg?itok=6kB0XMJx",
+    scores: ["2","4","1","2","2","1","2","5","5","3"]},
+    {name: "Happy Gilmore",
+    photo: "https://img.maximummedia.ie/joe_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE2XFxcLzEwXFxcLzA0MTgyNDA0XFxcL0hHLmpwZ1wiLFwid2lkdGhcIjo3NjcsXCJoZWlnaHRcIjo0MzEsXCJkZWZhdWx0XCI6XCJodHRwczpcXFwvXFxcL3d3dy5qb2UuaWVcXFwvYXNzZXRzXFxcL2ltYWdlc1xcXC9qb2VcXFwvbm8taW1hZ2UucG5nP3Y9NVwifSIsImhhc2giOiIyNDU2NWEzZjAzMjMxY2M5YjEyN2I1Y2FmNWEyNGI1NjRhOWRlMzk4In0=/hg.jpg",
+    scores: ["1","5","5","1","4","3","2","2","5","3"]},
+    {name: "Hulk",
+    photo: "https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/06/15/Pictures/_1caf1900-7098-11e8-bbf6-b72314b60444.jpg",
+    scores: ["4","3","2","3","4","1","3","5","5","2"]},
+    {name: "Loki",
+    photo: "https://www.sideshowtoy.com/wp-content/uploads/2018/03/marvel-thor-ragnarok-loki-sixth-scale-figure-hot-toys-feature-903106.jpg",
+    scores: ["5","4","1","2","3","1","4","3","2","5"]},
+    {name: "Spongebob",
+    photo: "https://www.washingtonpost.com/resizer/BCI261fnuv17vV2aCx5Nk5GLNhk=/480x0/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/RJYUEODFXQI6RAOKXMKFSOWKUY.jpg",
+    scores: ["3","2","4","3","2","4","5","5","2","4"]},
+    {name: "Goerge Costanza",
+    photo: "https://i.kym-cdn.com/photos/images/original/001/120/505/e35.png",
+    scores: ["1","4","5","2","2","1","1","1","1","1"]},
+    {name: "Busta Rhymes",
+    photo: "https://i1.wp.com/fatsaloon.com/wp-content/uploads/2016/09/busta-young.jpg?fit=600%2C622",
+    scores: ["1","4","5","2","2","5","2","4","3","4"]},
+  ]
+
+// Export the array
+module.exports = friends;
